f3e60f60d39e14f501ee34458b8f85fab655fb6e
松田知歩 <chiho.matsuda@rakus-partners.co.jp>
2022-08-02 14:17:34 +0900

    secound commit

---
+++ src/main/java/jp/co/sample/domain/Employee.java
@@ -0,0 +1,100 @@
+package jp.co.sample.domain;
+
+public class Employee {
+private Integer id;
+private String name;
+private String image;
+private String gender;
+private java.util.Date hireDate;
+private String mailAddress;
+private String zipCode;
+private String address;
+private String telehone;
+private Integer salary;
+private String characteristics;
+private Integer dependentsCount;
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getImage() {
+	return image;
+}
+public void setImage(String image) {
+	this.image = image;
+}
+public String getGender() {
+	return gender;
+}
+public void setGender(String gender) {
+	this.gender = gender;
+}
+public java.util.Date getHireDate() {
+	return hireDate;
+}
+public void setHireDate(java.util.Date hireDate) {
+	this.hireDate = hireDate;
+}
+public String getMailAddress() {
+	return mailAddress;
+}
+public void setMailAddress(String mailAddress) {
+	this.mailAddress = mailAddress;
+}
+public String getZipCode() {
+	return zipCode;
+}
+public void setZipCode(String zipCode) {
+	this.zipCode = zipCode;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getTelehone() {
+	return telehone;
+}
+public void setTelehone(String telehone) {
+	this.telehone = telehone;
+}
+public Integer getSalary() {
+	return salary;
+}
+public void setSalary(Integer salary) {
+	this.salary = salary;
+}
+public String getCharacteristics() {
+	return characteristics;
+}
+public void setCharacteristics(String characteristics) {
+	this.characteristics = characteristics;
+}
+public Integer getDependentsCount() {
+	return dependentsCount;
+}
+public void setDependentsCount(Integer dependentsCount) {
+	this.dependentsCount = dependentsCount;
+}
+@Override
+public String toString() {
+	return "Employee [id=" + id + ", name=" + name + ", image=" + image + ", gender=" + gender + ", hireDate="
+			+ hireDate + ", mailAddress=" + mailAddress + ", zipCode=" + zipCode + ", address=" + address
+			+ ", telehone=" + telehone + ", salary=" + salary + ", characteristics=" + characteristics
+			+ ", dependentsCount=" + dependentsCount + "]";
+}
+
+
+
+
+
+}
